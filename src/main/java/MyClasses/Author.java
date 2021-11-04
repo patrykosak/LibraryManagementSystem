a05d40ad4f1217da541e5a5ada32d184aeb92bbd
Patryk Osak <pionchaoo@gmail.com>
2021-11-04 22:09:26 +0100

    finish Author class

--- src/main/java/MyClasses/Author.java
+++ src/main/java/MyClasses/Author.java
@@ -11,4 +11,60 @@ package MyClasses;
  */
 public class Author {
     
+    private int id;
+    private String name;
+    private String surname;
+    private String fieldOfExpertise;
+    private String about;
+    
+    public Author(int id, String name,String surname,String fieldOfExpertise, String about){
+    
+        this.id=id;
+        this.name=name;
+        this.surname=surname;
+        this.fieldOfExpertise=fieldOfExpertise;
+        this.about=about;
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFieldOfExpertise() {
+        return fieldOfExpertise;
+    }
+
+    public void setFieldOfExpertise(String fieldOfExpertise) {
+        this.fieldOfExpertise = fieldOfExpertise;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+    
 }
